198d363e120ecae4d38d998a5a701b26acb5d2b9
FranciswOlsson <39450374+FranciswOlsson@users.noreply.github.com>
2018-06-25 17:09:23 -0700

    Add files via upload

---
+++ Practice Problem.cpp
@@ -0,0 +1,20 @@
+//Francis Olsson
+//CISP 360
+//Practice problem
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string fname; 
+
+int main(){
+    cout << "Please enter your first name: ";
+    cin >> fname;
+    cout << "Welcome to CISP 360 - Introduction to C++ programming " << fname << endl;
+    system("pause");
+    return 0;
+}
+
+    
