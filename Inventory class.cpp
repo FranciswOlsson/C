c1514adaabb1c34a4e20295235d6d976ac89e1ec
FranciswOlsson <39450374+FranciswOlsson@users.noreply.github.com>
2018-08-17 21:29:35 -0700

    Add files via upload

---
+++ Inventory class.cpp
@@ -0,0 +1,20 @@
+//Francis Olsson
+//CISP 360
+//Inventory Class assignment
+/*Program description:
+
+
+
+*/
+    
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	cout << 5%13;
+    system("pause");
+    return 0;
+}
